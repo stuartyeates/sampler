f855aab5d8dd6d6206623f511930719273cac2a5
stuart yeates <syeates@gmail.com>
2013-11-21 11:08:01 +1300

    fix special chars

--- tidy.sh
+++ tidy.sh
@@ -5,13 +5,22 @@ do
     URL=`echo $URL1 | sed 's^|^%7C^g'`
     #echo $URL $URL1
     if [ -n "$URL" ]; then 
-	LOCATION=`curl --silent --retry 1 --head $URL| grep 'Location:'|sed 's^Location: ^^g' `
+	LOCATION=`curl --silent --retry 1 --head $URL| grep 'Location:'|sed 's^Location: ^^g' |tr -d '\15\32'`
 	#echo $LOCATION
 	if [ -n "$LOCATION" ]; then 
 	    URL=$LOCATION
+	    LOCATION=`curl --silent --retry 1 --head $URL| grep 'Location:'|sed 's^Location: ^^g' |tr -d '\15\32'`
+	    if [ -n "$LOCATION" ]; then 
+		URL=$LOCATION
+		LOCATION=`curl --silent --retry 1 --head $URL| grep 'Location:'|sed 's^Location: ^^g' |tr -d '\15\32'`
+		if [ -n "$LOCATION" ]; then 
+		    URL=$LOCATION
+		fi
+	    fi
 	fi
-	SIZE=`curl --silent --retry 1 --head $URL| grep 'Content-Length:'| sed 's^Content-Length: ^^g'`
+	SIZE=`curl --silent --retry 1 --head $URL| grep 'Content-Length:'| sed 's^Content-Length: ^ ^g'|tr -d '\15\32'`
 	#echo $SIZE;
-	echo $URL, $SIZE,
+	echo $URL1, $URL, $SIZE,
+	
     fi
 done
